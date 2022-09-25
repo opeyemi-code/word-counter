ab2c778f101f946bcf9a924e964e271c1deb1da1
Opeyemi <84101630+opeyemi-code@users.noreply.github.com>
2022-09-25 20:34:13 +0100

    initial file upload

---
+++ main.js
@@ -0,0 +1,35 @@
+const textArea = document.querySelector('#textarea');
+const countBtn = document.querySelector('#count-btn');
+const result = document.querySelector('#result');
+const clearBtn = document.querySelector('#clear-btn')
+
+countBtn.addEventListener('click', wordCounter);
+
+clearBtn.addEventListener('click', clearTextArea);
+
+function wordCounter (e){
+ const textAreaLength = textArea.value.
+ split(" ").filter(word => word.match(/[a-z0-9]/gi)).length;
+ if (textArea.value === ""){
+   alert('You have not written anything yet');
+ }else{
+   
+ if (textAreaLength === 1){
+ result.textContent = `${textAreaLength} word`;
+}
+ else{
+   result.textContent = `${textAreaLength} words`;
+ }
+  countBtn.setAttribute('style','display: none');
+ clearBtn.setAttribute('style','display: block');
+}
+ e.preventDefault();
+}
+
+function clearTextArea (e) {
+  textArea.value = "";
+   clearBtn.setAttribute('style','display: none');
+   countBtn.setAttribute('style','display: block');
+
+  e.preventDefault;
+}
